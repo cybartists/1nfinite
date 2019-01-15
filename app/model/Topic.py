f3363fe193ec7e051652c116571127c89c2fdece
擎天柱 <840012812@qq.com>
2019-01-15 20:52:02 +0800

    擎天柱

--- app/model/Topic.py
+++ app/model/Topic.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column,INT,VARCHAR,Text,Boolean
+from sqlalchemy import Column,INT,VARCHAR,Text,Boolean,ForeignKey
 from sqlalchemy.orm import relationship
 
 from app.base.extensions import Base
@@ -7,7 +7,7 @@ from app.base.extensions import Base
 class Topic(Base):
     __tablename__ = 'Topic'
     id = Column(INT, primary_key=True, autoincrement=True)
-    user_id = Column(INT,)
+    user_id = Column(INT,ForeignKey("user.id"))
     title = Column(Text)
     sum = Column(Text)
 
