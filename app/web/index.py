f5c565fd458dde24bcb66074a778ce464f24dd51
chisec <chsiecisme@gmail.com>
2019-01-16 04:37:11 +0800

    wtf

--- app/web/index.py
+++ app/web/index.py
@@ -2,8 +2,6 @@ from flask import render_template, session
 
 from app.base.function import is_login, is_admin
 from app.web import web
-from app.base.extensions import DBSession
-from app.model.User import User
 
 
 @web.route('/')
