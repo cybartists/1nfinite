95d95ba9e263449ee16bfcafc5ec83e81f605131
擎天柱 <840012812@qq.com>
2019-01-15 21:37:09 +0800

    擎天柱

--- app/model/Channel.py
+++ app/model/Channel.py
@@ -7,5 +7,5 @@ from app.base.extensions import Base
 class Channel(Base):
     __tablename__ = 'Channel'
     id = Column(INT, primary_key=True, autoincrement=True)
-    use_id = Column(INT,ForeignKey("user.id"))
+    user_id = Column(INT,ForeignKey("user.id"))
     Content = Column(Text)
