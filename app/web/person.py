6390af9d119c7d5be214ee954b1007ee47bf7cd1
xzFunter <37768764+xzFunter@users.noreply.github.com>
2019-01-17 22:52:45 +0800

    push

--- app/web/person.py
+++ app/web/person.py
@@ -9,3 +9,23 @@ from app.model.User import User
 @web.route('/person/')
 def person():
     return render_template('person.html', login=is_login())
+
+
+@web.route('/person_follow/')
+def person_follow():
+    return render_template('person_follow.html', login=is_login())
+
+
+@web.route('/person_like/')
+def person_like():
+    return render_template('person_like.html', login=is_login())
+
+
+@web.route('/person_info/')
+def person_info():
+    return render_template('person_info.html', login=is_login())
+
+
+@web.route('/person_about/')
+def person_about():
+    return render_template('person_about.html', login=is_login())
