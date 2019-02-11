4b74e77c3d97b02af1dc5832fd7bc5d6cb3d5a85
chisec <chsiecisme@gmail.com>
2019-02-12 00:32:10 +0800

    fix bugs.

--- manager.py
+++ manager.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
 # -*- coding:utf-8 -*-
 from app import create_app
-from flask_script import Manager
+from flask_script import Manager, Server
 
 app = create_app()
 manager = Manager(app)
+
+
+@manager.command
+def debug():
+    Server(debugger=True)
 
 
 if __name__ == '__main__':
